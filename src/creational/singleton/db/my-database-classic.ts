c3255053ce148497da0cc33ba7e483e7391a27e2
Lucas <lucas.silva1037@gmail.com>
2023-10-25 18:12:38 -0300

    up

--- src/creational/singleton/db/my-database-classic.ts
+++ src/creational/singleton/db/my-database-classic.ts
@@ -1,30 +1,30 @@
 import { User } from '../interfaces/user';
 
 export class MyDatabaseClassic {
-  private static _instance: MyDatabaseClassic | null = null;
-  private users: User[] = [];
+    private static _instance: MyDatabaseClassic | null = null;
+    private users: User[] = [];
 
-  private constructor() {}
+    private constructor() {}
 
-  static get instance(): MyDatabaseClassic {
-    if (MyDatabaseClassic._instance === null) {
-      MyDatabaseClassic._instance = new MyDatabaseClassic();
+    static get instance(): MyDatabaseClassic {
+        if (MyDatabaseClassic._instance === null) {
+            MyDatabaseClassic._instance = new MyDatabaseClassic();
+        }
+
+        return MyDatabaseClassic._instance;
     }
 
-    return MyDatabaseClassic._instance;
-  }
-
-  add(user: User): void {
-    this.users.push(user);
-  }
-
-  remove(index: number): void {
-    this.users.splice(index, 1);
-  }
-
-  show(): void {
-    for (const user of this.users) {
-      console.log(user);
+    add(user: User): void {
+        this.users.push(user);
     }
-  }
+
+    remove(index: number): void {
+        this.users.splice(index, 1);
+    }
+
+    show(): void {
+        for (const user of this.users) {
+            console.log(user);
+        }
+    }
 }
